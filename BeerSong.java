040afbee96df2988ddbd442856deff6a0579bbd2
Chris DePalma <cndepalma@oakland.edu>
2017-01-29 13:12:15 -0500

    Added empty Main() and Ninety_nine_Bottles_of_Beer() functions

---
+++ BeerSong.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beersong;
+
+/**
+ *
+ * @author Chris
+ */
+public class BeerSong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public  void main(String[] args) {
+        Ninety_Nine_Bottles_of_Beer();
+    }
+    public void Ninety_Nine_Bottles_of_Beer(){
+        
+    }
+    
+}
